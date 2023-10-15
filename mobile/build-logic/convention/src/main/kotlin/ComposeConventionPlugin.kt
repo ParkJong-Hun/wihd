42ec7da8dfe91dd446ff18e62aa617041f8c0542
ParkJong-Hun <thesn515@gmail.com>
2023-10-15 21:17:40 +0900

    add: implement compose convention plugin

--- mobile/build-logic/convention/src/main/kotlin/ComposeConventionPlugin.kt
+++ mobile/build-logic/convention/src/main/kotlin/ComposeConventionPlugin.kt
@@ -1,8 +1,42 @@
+import dsl.android
+import dsl.debugImplementation
+import dsl.implementation
+import dsl.library
+import dsl.libs
+import dsl.lintChecks
+import dsl.testImplementation
+import dsl.version
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class ComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        TODO("Not yet implemented")
+        with(target) {
+            android {
+                buildFeatures.compose = true
+                composeOptions {
+                    kotlinCompilerExtensionVersion = libs.version("composeCompiler")
+                }
+            }
+            dependencies {
+                implementation(platform(libs.library("composeBom")))
+                implementation(libs.library("androidxCoreKtx"))
+                implementation(libs.library("composeUi"))
+                implementation(libs.library("composeMaterial"))
+                implementation(libs.library("composeMaterial3"))
+                implementation(libs.library("composeUiToolingPreview"))
+                implementation(libs.library("androidxLifecycleLifecycleRuntimeKtx"))
+                implementation(libs.library("androidxActivityActivityCompose"))
+                implementation(libs.library("lottieCompose"))
+                testImplementation(libs.library("junit"))
+                testImplementation(libs.library("androidxTestExtJunit"))
+                testImplementation(libs.library("androidxTestEspressoEspressoCore"))
+                testImplementation(libs.library("composeUiTestJunit4"))
+                debugImplementation(libs.library("composeUiTooling"))
+                debugImplementation(libs.library("composeUiTestManifest"))
+                lintChecks(libs.library("composeLintCheck"))
+            }
+        }
     }
 }
