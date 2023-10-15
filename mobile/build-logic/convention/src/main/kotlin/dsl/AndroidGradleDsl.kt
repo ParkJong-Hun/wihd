d50c1a174bee1ce19989250f8354727a6b7d1523
ParkJong-Hun <thesn515@gmail.com>
2023-10-15 21:17:19 +0900

    add: create dsl extension

---
+++ mobile/build-logic/convention/src/main/kotlin/dsl/AndroidGradleDsl.kt
@@ -0,0 +1,66 @@
+package dsl
+
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.TestedExtension
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+fun Project.androidApplication(action: BaseAppModuleExtension.() -> Unit) {
+    extensions.configure(action)
+}
+
+fun Project.androidLibrary(action: LibraryExtension.() -> Unit) {
+    extensions.configure(action)
+}
+
+fun Project.android(action: TestedExtension.() -> Unit) {
+    extensions.configure(action)
+}
+
+fun Project.setupAndroid() {
+    android {
+        namespace?.let {
+            this.namespace = it
+        }
+        compileSdkVersion(34)
+
+        defaultConfig {
+            minSdk = 23
+            targetSdk = 33
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+            isCoreLibraryDesugaringEnabled = true
+        }
+        dependencies {
+            add("coreLibraryDesugaring", libs.library("androidDesugarJdkLibs"))
+        }
+        testOptions {
+            unitTests {
+                isIncludeAndroidResources = true
+            }
+        }
+
+        (this as CommonExtension<*, *, *, *, *>).lint {
+            val filename = displayName.replace(":", "_").replace("[\\s']".toRegex(), "")
+
+            xmlReport = true
+            xmlOutput =
+                rootProject.layout.buildDirectory.file("lint-reports/lint-results-${filename}.xml")
+                    .get().asFile
+
+            htmlReport = true
+            htmlOutput =
+                rootProject.layout.buildDirectory.file("lint-reports/lint-results-${filename}.html")
+                    .get().asFile
+
+            sarifReport = false
+        }
+    }
+}
