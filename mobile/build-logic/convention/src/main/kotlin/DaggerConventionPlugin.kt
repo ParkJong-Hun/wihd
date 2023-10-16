8501c34183e44694337ecb2398bc518f6a1dfed2
ParkJong-Hun <thesn515@gmail.com>
2023-10-16 22:02:09 +0900

    add: implement dagger convention plugin

--- mobile/build-logic/convention/src/main/kotlin/DaggerConventionPlugin.kt
+++ mobile/build-logic/convention/src/main/kotlin/DaggerConventionPlugin.kt
@@ -1,8 +1,32 @@
+import dsl.android
+import dsl.implementation
+import dsl.ksp
+import dsl.library
+import dsl.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class DaggerConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        TODO("Not yet implemented")
+        with(target) {
+            with(pluginManager) {
+                apply("com.google.devtools.ksp")
+                apply("dagger.hilt.android.plugin")
+            }
+
+            android {
+                packagingOptions {
+                    resources {
+                        excludes += "META-INF/gradle/incremental.annotation.processors"
+                    }
+                }
+            }
+            dependencies {
+                implementation(libs.library("dagger-hilt"))
+                // https://issuetracker.google.com/issues/237567009
+                ksp(libs.library("dagger-hilt-compiler"))
+            }
+        }
     }
 }
