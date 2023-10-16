e63235b3a672efd1631d185612294af558aa3e48
ParkJong-Hun <thesn515@gmail.com>
2023-10-16 22:03:08 +0900

    add: implement firebase convention plugin

--- mobile/build-logic/convention/src/main/kotlin/FirebaseConventionPlugin.kt
+++ mobile/build-logic/convention/src/main/kotlin/FirebaseConventionPlugin.kt
@@ -1,8 +1,28 @@
+import dsl.android
+import dsl.implementationPlatform
+import dsl.library
+import dsl.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class FirebaseConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        TODO("Not yet implemented")
+        with(target) {
+            with(pluginManager) {
+                apply("com.google.gms.google-services")
+            }
+
+            android {
+                packagingOptions {
+                    resources {
+                        excludes += "META-INF/gradle/incremental.annotation.processors"
+                    }
+                }
+            }
+            dependencies {
+                implementationPlatform(libs.library("firebase-bom"))
+            }
+        }
     }
 }
