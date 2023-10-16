06da9921459291553a0a65d5c8e674635f56eef7
ParkJong-Hun <thesn515@gmail.com>
2023-10-16 22:16:15 +0900

    edit: delete test convention plugin file

--- mobile/build-logic/convention/src/main/kotlin/TestConventionPlugin.kt
+++
@@ -1,8 +0,0 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-
-class TestConventionPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        TODO("Not yet implemented")
-    }
-}
