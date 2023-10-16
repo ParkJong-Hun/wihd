8494571b999d75734639675f250eb93fa99c758c
ParkJong-Hun <thesn515@gmail.com>
2023-10-16 22:18:20 +0900

    add: add kotlin convention plugin file

---
+++ mobile/build-logic/convention/src/main/kotlin/KotlinConventionPlugin.kt
@@ -0,0 +1,8 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class KotlinConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
