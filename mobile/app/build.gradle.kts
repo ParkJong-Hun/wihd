2d99fc6f390466f97659e7f64843b6bd64419156
ParkJong-Hun <thesn515@gmail.com>
2023-10-16 22:28:56 +0900

    add: implement kotlin convention plugin

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -1,22 +1,17 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidGradlePlugin)
-    alias(libs.plugins.kotlinPlugin)
-    alias(libs.plugins.kspPlugin)
+    id("parkjonghun.whatishedoingwithandroid.mobile.convention.core")
     id("parkjonghun.whatishedoingwithandroid.mobile.convention.compose")
     id("parkjonghun.whatishedoingwithandroid.mobile.convention.dagger")
     id("parkjonghun.whatishedoingwithandroid.mobile.convention.firebase")
+    id("parkjonghun.whatishedoingwithandroid.mobile.convention.kotlin")
 }
 
 android {
     namespace = "co.kr.parkjonghun.whatishedoingwithandroid.mobile"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "co.kr.parkjonghun.whatishedoingwithandroid.mobile"
-        minSdk = 29
-        // noinspection EditedTargetSdkVersion
-        targetSdk = 34
         versionCode = 1
         versionName = "0.1.0.01"
 
@@ -34,13 +29,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
     packaging {
         resources {
--- mobile/build-logic/convention/src/main/kotlin/KotlinConventionPlugin.kt
+++ mobile/build-logic/convention/src/main/kotlin/KotlinConventionPlugin.kt
@@ -1,8 +1,24 @@
+import dsl.android
+import dsl.kotlinOptions
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class KotlinConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        TODO("Not yet implemented")
+        with(target) {
+            with(pluginManager) {
+                apply("org.jetbrains.kotlin.android")
+            }
+            tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java) {
+                kotlinOptions.jvmTarget = "17"
+            }
+
+            android {
+                kotlinOptions {
+                    jvmTarget = JavaVersion.VERSION_17.toString()
+                }
+            }
+        }
     }
 }
