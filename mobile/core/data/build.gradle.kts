e5341c969e5bbcde34b61843e47603844c24323a
ParkJong-Hun <thesn515@gmail.com>
2023-10-25 22:50:36 +0900

    chore: change to custom plugin

--- mobile/core/data/build.gradle.kts
+++ mobile/core/data/build.gradle.kts
@@ -1,37 +1,10 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidGradleLibraryPlugin)
-    alias(libs.plugins.kotlinPlugin)
+    id("parkjonghun.whatishedoingwithandroid.mobile.convention.library")
+    id("parkjonghun.whatishedoingwithandroid.mobile.convention.android")
+    id("parkjonghun.whatishedoingwithandroid.mobile.convention.dagger")
 }
 
-android {
-    namespace = "co.kr.parkjonghun.whatishedoingwithandroid.data"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 29
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
+android.namespace = "co.kr.parkjonghun.whatishedoingwithandroid.data"
 
 dependencies {
     implementation(project(":core:domain"))
