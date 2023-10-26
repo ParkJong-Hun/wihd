f24506ed2f83dd2bdf5619168cd39508f92d9662
ParkJong-Hun <thesn515@gmail.com>
2023-10-26 22:56:33 +0900

    feat: use hiltViewModel

--- mobile/feature/main/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/main/ui/MainScreen.kt
+++ mobile/feature/main/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/main/ui/MainScreen.kt
@@ -1,8 +1,11 @@
 package co.kr.parkjonghun.whatishedoingwithandroid.main.ui
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    viewModel: MainViewModel = hiltViewModel(),
+) {
 
 }
