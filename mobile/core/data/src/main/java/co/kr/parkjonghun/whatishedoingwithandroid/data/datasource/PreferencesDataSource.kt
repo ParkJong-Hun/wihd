e5618d26bc2d4ea5e1bb9829d25e325737822023
ParkJong-Hun <thesn515@gmail.com>
2023-10-31 23:38:37 +0900

    feat: implement PreferencesDataSource

--- mobile/core/data/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/data/datasource/PreferencesDataSource.kt
+++ mobile/core/data/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/data/datasource/PreferencesDataSource.kt
@@ -1,7 +1,13 @@
 package co.kr.parkjonghun.whatishedoingwithandroid.data.datasource
 
-// TODO use dataStorePref
-class PreferencesDataSource(
-) {
+import android.content.Context
+import co.kr.parkjonghun.whatishedoingwithandroid.data.extension.sampleDataStore
+import co.kr.parkjonghun.whatishedoingwithandroid.data.extension.userDataStore
+import javax.inject.Inject
 
+class PreferencesDataSource @Inject constructor(
+    private val context: Context
+) {
+    private val sampleDataStore get() = context.sampleDataStore
+    private val userDataStore get() = context.userDataStore
 }
