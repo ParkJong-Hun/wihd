32d9e87ad4e16e967adc35bb02a8c8f9b0eee72f
ParkJong-Hun <thesn515@gmail.com>
2023-11-18 23:45:55 +0900

    feat: impl app state

--- mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/WihdApp.kt
+++ mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/WihdApp.kt
@@ -1,6 +1,5 @@
 package co.kr.parkjonghun.whatishedoingwithandroid.mobile
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
@@ -13,14 +12,17 @@ import androidx.navigation.compose.rememberNavController
 import co.kr.parkjonghun.whatishedoingwithandroid.main.ui.mainScreen
 import co.kr.parkjonghun.whatishedoingwithandroid.ui.theme.MobileTheme
 
+/**
+ * Top level composable.
+ */
 @Composable
 fun WihdApp(
     windowSizeClass: WindowSizeClass,
-    modifier: Modifier = Modifier,
+    appState: WihdAppState = rememberWihdAppState(windowSizeClass = windowSizeClass),
 ) {
     MobileTheme {
         Surface(
-            modifier = modifier.fillMaxSize(),
+            modifier = Modifier,
             color = MaterialTheme.colorScheme.background,
         ) {
             WihdNavHost()
