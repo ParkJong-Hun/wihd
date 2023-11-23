8efcaf29c6cae5c3757111fbbd0c14b8daaf9d19
ParkJong-Hun <thesn515@gmail.com>
2023-11-23 22:36:11 +0900

    feat: navigation rail item enum

---
+++ mobile/feature/main/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/main/ui/navigation/MainNavigationRailItem.kt
@@ -0,0 +1,13 @@
+package co.kr.parkjonghun.whatishedoingwithandroid.main.ui.navigation
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class MainNavigationRailItem(
+    val icon: ImageVector,
+    val selectedIcon: ImageVector,
+    @StringRes val iconTextRes: Int,
+    @StringRes val labelTextRes: Int,
+) {
+    // TODO
+}
