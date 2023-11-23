297c6a9516c816cebc175d4dd9d67dd36787b2eb
ParkJong-Hun <thesn515@gmail.com>
2023-11-23 22:36:21 +0900

    feat: app naivgation rail

---
+++ mobile/feature/main/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/main/ui/WhidNavigationRail.kt
@@ -0,0 +1,50 @@
+package co.kr.parkjonghun.whatishedoingwithandroid.main.ui
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import co.kr.parkjonghun.whatishedoingwithandroid.main.ui.navigation.MainNavigationRailItem
+
+@Composable
+fun WhidNavigationRail(
+    // TODO: to ImmutableList
+    mainRailItems: List<MainNavigationRailItem>,
+    onRailItemSelected: (MainNavigationRailItem) -> Unit,
+    selectedRailItem: MainNavigationRailItem,
+    modifier: Modifier = Modifier,
+) {
+    NavigationRail(
+        modifier = modifier,
+    ) {
+        mainRailItems.forEach { item ->
+            val isCurrentRailItem = item == selectedRailItem
+            val contentDescription = stringResource(id = item.iconTextRes)
+            NavigationRailItem(
+                selected = item == selectedRailItem,
+                onClick = { onRailItemSelected(item) },
+                icon = {
+                    if (isCurrentRailItem) {
+                        Icon(
+                            imageVector = item.selectedIcon,
+                            contentDescription = contentDescription,
+                        )
+                    } else {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = contentDescription
+                        )
+                    }
+                },
+                label = {
+                    Text(
+                        text = stringResource(id = item.labelTextRes),
+                    )
+                }
+            )
+        }
+    }
+}
