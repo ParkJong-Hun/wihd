3e97af34efc72a962d56010d767c312b3d11410e
ParkJong-Hun <thesn515@gmail.com>
2023-11-27 20:51:48 +0900

    fix: navigation rail rename, move

--- mobile/feature/main/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/main/ui/navigation/MainNavigationRail.kt
+++ mobile/feature/main/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/main/ui/navigation/MainNavigationRail.kt
@@ -1,4 +1,4 @@
-package co.kr.parkjonghun.whatishedoingwithandroid.main.ui
+package co.kr.parkjonghun.whatishedoingwithandroid.main.ui.navigation
 
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationRail
@@ -7,10 +7,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import co.kr.parkjonghun.whatishedoingwithandroid.main.ui.navigation.MainNavigationRailItem
 
 @Composable
-fun WhidNavigationRail(
+fun MainNavigationRail(
     // TODO: to ImmutableList
     mainRailItems: List<MainNavigationRailItem>,
     onRailItemSelected: (MainNavigationRailItem) -> Unit,
