95812e4afd63962c6a69163f8a1966a8785d5d65
ParkJong-Hun <thesn515@gmail.com>
2023-12-08 21:09:12 +0900

    chore: use supabase on data module

--- mobile/core/data/build.gradle.kts
+++ mobile/core/data/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.wihd.library)
     alias(libs.plugins.wihd.dagger)
     alias(libs.plugins.wihd.detekt)
+    alias(libs.plugins.wihd.serialization)
 }
 
 android.namespace = "co.kr.parkjonghun.whatishedoingwithandroid.data"
@@ -12,6 +13,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.jetpack.datastore.preferences)
+    implementation(libs.ktor)
+    implementation(libs.supabase)
     testImplementation(libs.junit)
     androidTestImplementation(libs.jetpack.junit)
     androidTestImplementation(libs.jetpack.espresso.core)
