102779965ff0406452b7fc2ec986d93c87ec1ee4
ParkJong-Hun <thesn515@gmail.com>
2023-12-10 14:51:22 +0900

    feat: use parcelize

--- mobile/core/domain/build.gradle.kts
+++ mobile/core/domain/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.wihd.library)
     alias(libs.plugins.wihd.dagger)
     alias(libs.plugins.wihd.detekt)
+    alias(libs.plugins.wihd.parcelize)
 }
 
 android.namespace = "co.kr.parkjonghun.whatishedoingwithandroid.domain"
