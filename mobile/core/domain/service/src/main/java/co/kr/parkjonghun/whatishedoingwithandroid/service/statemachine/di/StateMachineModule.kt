45abc7d7f43bd2631b19dbab7e85d28d010104a3
ParkJong-Hun <thesn515@gmail.com>
2023-12-12 19:32:13 +0900

    fix: mistake

--- mobile/core/domain/service/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/service/statemachine/di/StateMachineModule.kt
+++ mobile/core/domain/service/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/service/statemachine/di/StateMachineModule.kt
@@ -3,11 +3,13 @@ package co.kr.parkjonghun.whatishedoingwithandroid.service.statemachine.di
 import co.kr.parkjonghun.whatishedoingwithandroid.service.gateway.repository.SampleRepository
 import co.kr.parkjonghun.whatishedoingwithandroid.service.statemachine.sample.SampleSideEffectCreator
 import co.kr.parkjonghun.whatishedoingwithandroid.service.statemachine.sample.createSampleStateMachine
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 
+@Module
 @InstallIn(SingletonComponent::class, ViewModelComponent::class)
 @Suppress("unused")
 class StateMachineModule {
