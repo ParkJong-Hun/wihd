34a893923840236eb558f4d72fea1be15e4e81bb
ParkJong-Hun <thesn515@gmail.com>
2023-12-14 22:14:55 +0900

    fix: delete hilt code

--- mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/MainActivity.kt
+++ mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
