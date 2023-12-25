368b068f452e7fd9d89f243441d2e8e7acea9691
Park JongHun <thesn515@gmail.com>
2023-12-25 22:36:17 +0900

    fix: not plugin, lib

--- mobile/build-logic/convention/src/main/kotlin/FeatureBundlePlugin.kt
+++ mobile/build-logic/convention/src/main/kotlin/FeatureBundlePlugin.kt
@@ -1,5 +1,9 @@
+import dsl.implementation
+import dsl.library
+import dsl.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 @Suppress("unused")
 class FeatureBundlePlugin : Plugin<Project> {
@@ -9,8 +13,11 @@ class FeatureBundlePlugin : Plugin<Project> {
                 apply("parkjonghun.whatishedoingwithandroid.mobile.convention.library")
                 apply("parkjonghun.whatishedoingwithandroid.mobile.convention.kotlin")
                 apply("parkjonghun.whatishedoingwithandroid.mobile.convention.compose")
-                apply("parkjonghun.whatishedoingwithandroid.mobile.convention.koin")
                 apply("parkjonghun.whatishedoingwithandroid.mobile.convention.detekt")
+            }
+
+            dependencies {
+                implementation(libs.library("koin-compose"))
             }
         }
     }
