e99cc401c7a6595e23c796cd00c0fc2762fe39e6
Park JongHun <thesn515@gmail.com>
2023-12-26 21:46:36 +0900

    fix: ui state : domain state is not 1 : 1

--- mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/AppUiState.kt
+++ mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/AppUiState.kt
@@ -6,15 +6,32 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import co.kr.parkjonghun.whatishedoingwithandroid.base.usecase.statemachine.StateMachine
 import co.kr.parkjonghun.whatishedoingwithandroid.service.gateway.repository.dto.presentation.TokenDto
 import co.kr.parkjonghun.whatishedoingwithandroid.service.usecase.statemachine.app.AppAction
 import co.kr.parkjonghun.whatishedoingwithandroid.service.usecase.statemachine.app.AppState
 import co.kr.parkjonghun.whatishedoingwithandroid.ui.base.Intent
 import co.kr.parkjonghun.whatishedoingwithandroid.ui.base.UiState
-import kotlinx.coroutines.flow.map
-import org.koin.compose.rememberKoinInject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import org.koin.compose.koinInject
 import org.koin.core.qualifier.named
+
+@Composable
+fun rememberAppUiState(): Pair<State<AppUiState>, AppIntent> {
+    val scope = rememberCoroutineScope()
+    val stateMachine = koinInject<StateMachine<AppState, AppAction>>(
+        qualifier = named("App"),
+    )
+    val state = remember { MutableStateFlow(AppUiState()) }
+    val intent = remember { AppIntent(stateMachine) }
+    remember { AppReducer(stateMachine, state, scope) }
+    return state.collectAsState() to intent
+}
 
 @Immutable
 data class AppUiState(
@@ -25,75 +42,6 @@ data class AppUiState(
 ) : UiState {
     val isShowError: Boolean = error != null
     val isShowTop: Boolean = !isShowLoginScreen && !isShowInitScreen
-}
-
-@Composable
-fun rememberAppUiState(): Pair<State<AppUiState>, AppIntent> {
-    val stateMachine =
-        rememberKoinInject<StateMachine<AppState, AppAction>>(qualifier = named("App"))
-    val state = remember {
-        AppReducer(stateMachine).appUiState
-    }.collectAsState(
-        initial = AppUiState(),
-    )
-    val intent = remember {
-        AppIntent(stateMachine)
-    }
-    return state to intent
-}
-
-@Stable
-class AppReducer(
-    stateMachine: StateMachine<AppState, AppAction>,
-) {
-    val appUiState = stateMachine.flow.map { domainState ->
-        when (domainState) {
-            is AppState.None -> {
-                AppUiState(
-                    isShowInitScreen = true,
-                    isShowLoading = false,
-                    isShowLoginScreen = false,
-                    error = null,
-                )
-            }
-
-            is AppState.Loading -> {
-                AppUiState(
-                    isShowInitScreen = true,
-                    isShowLoading = true,
-                    isShowLoginScreen = false,
-                    error = null,
-                )
-            }
-
-            is AppState.LoggedIn -> {
-                AppUiState(
-                    isShowInitScreen = false,
-                    isShowLoading = false,
-                    isShowLoginScreen = false,
-                    error = null,
-                )
-            }
-
-            is AppState.NotLoggedIn -> {
-                AppUiState(
-                    isShowInitScreen = false,
-                    isShowLoading = false,
-                    isShowLoginScreen = true,
-                    error = null,
-                )
-            }
-
-            is AppState.Error -> {
-                AppUiState(
-                    isShowInitScreen = true,
-                    isShowLoading = false,
-                    error = domainState.throwable,
-                    isShowLoginScreen = false,
-                )
-            }
-        }
-    }
 }
 
 class AppIntent(
@@ -109,5 +57,75 @@ class AppIntent(
 
     private fun checkUser() {
         stateMachine.dispatch(AppAction.CheckLogin)
+    }
+}
+
+@Stable
+class AppReducer(
+    stateMachine: StateMachine<AppState, AppAction>,
+    private val uiState: MutableStateFlow<AppUiState>,
+    coroutineScope: CoroutineScope,
+) {
+    init {
+        stateMachine.flow
+            .onEach { domainState ->
+                when (domainState) {
+                    is AppState.None -> {
+                        uiState.update {
+                            it.copy(
+                                isShowInitScreen = true,
+                                isShowLoading = false,
+                                isShowLoginScreen = false,
+                                error = null,
+                            )
+                        }
+                    }
+
+                    is AppState.Loading -> {
+                        uiState.update {
+                            it.copy(
+                                isShowInitScreen = true,
+                                isShowLoading = true,
+                                isShowLoginScreen = false,
+                                error = null,
+                            )
+                        }
+                    }
+
+                    is AppState.LoggedIn -> {
+                        uiState.update {
+                            it.copy(
+                                isShowInitScreen = false,
+                                isShowLoading = false,
+                                isShowLoginScreen = false,
+                                error = null,
+                            )
+                        }
+                    }
+
+                    is AppState.NotLoggedIn -> {
+                        uiState.update {
+                            it.copy(
+                                isShowInitScreen = false,
+                                isShowLoading = false,
+                                isShowLoginScreen = true,
+                                error = null,
+                            )
+                        }
+                    }
+
+                    is AppState.Error -> {
+                        uiState.update {
+                            it.copy(
+                                isShowInitScreen = true,
+                                isShowLoading = false,
+                                error = domainState.throwable,
+                                isShowLoginScreen = false,
+                            )
+                        }
+                    }
+                }
+            }
+            .launchIn(coroutineScope)
     }
 }
