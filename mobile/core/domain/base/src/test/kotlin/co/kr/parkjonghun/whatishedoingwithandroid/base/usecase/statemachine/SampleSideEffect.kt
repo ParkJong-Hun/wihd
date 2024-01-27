ea6abbe30783e5abcac019efed6524d3cc1f23d8
Park JongHun <thesn515@gmail.com>
2024-01-27 11:29:03 +0900

    fix: lint

--- mobile/core/domain/base/src/test/kotlin/co/kr/parkjonghun/whatishedoingwithandroid/base/usecase/statemachine/SampleSideEffect.kt
+++ mobile/core/domain/base/src/test/kotlin/co/kr/parkjonghun/whatishedoingwithandroid/base/usecase/statemachine/SampleSideEffect.kt
@@ -3,7 +3,7 @@ package co.kr.parkjonghun.whatishedoingwithandroid.base.usecase.statemachine
 class SampleSideEffect : SideEffect<SampleState, SampleAction> {
     override suspend fun fire(
         targetStateMachine: StateMachine<SampleState, SampleAction>,
-        validTransition: ValidTransition<SampleState, SampleAction>
+        validTransition: ValidTransition<SampleState, SampleAction>,
     ) {
         runCatching { "Sample Data" }
             .onSuccess { targetStateMachine.dispatch(SampleAction.Succeed(it)) }
