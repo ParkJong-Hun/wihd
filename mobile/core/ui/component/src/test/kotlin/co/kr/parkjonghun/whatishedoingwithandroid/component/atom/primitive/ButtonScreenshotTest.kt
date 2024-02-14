19a5d1897d7c010e2fdcd89f0b863295754194e1
Park JongHun <thesn515@gmail.com>
2024-02-14 21:27:21 +0900

    feat: button screenshot test(not yet)

---
+++ mobile/core/ui/component/src/test/kotlin/co/kr/parkjonghun/whatishedoingwithandroid/component/atom/primitive/ButtonScreenshotTest.kt
@@ -0,0 +1,19 @@
+package co.kr.parkjonghun.whatishedoingwithandroid.component.atom.primitive
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ButtonScreenshotTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun test_sample() {
+        // TODO
+    }
+}
