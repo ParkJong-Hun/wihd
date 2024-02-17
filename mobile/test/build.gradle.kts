b0b8c5ff30435288d38ec59498c1a3e7bc77be7e
Park JongHun <thesn515@gmail.com>
2024-02-17 19:21:56 +0900

    chore: delete test files

--- mobile/test/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    alias(libs.plugins.wihd.library)
-    alias(libs.plugins.wihd.kotlin)
-    alias(libs.plugins.wihd.test)
-    alias(libs.plugins.wihd.detekt)
-}
-
-android.namespace = "co.kr.parkjonghun.whatishedoingwithandroid.mobile.test"
