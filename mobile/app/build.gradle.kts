627eeaca3b040d3a30994d2f03cbcd7929cd3739
Park JongHun <thesn515@gmail.com>
2024-02-23 11:48:26 +0900

    chore: bump app version to 0.2.0

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "co.kr.parkjonghun.whatishedoingwithandroid.mobile"
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
