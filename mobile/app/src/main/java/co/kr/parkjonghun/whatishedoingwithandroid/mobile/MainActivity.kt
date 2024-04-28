df9be6f400399b6e899fa403837e3258d134f36d
Park JongHun <thesn515@gmail.com>
2024-04-28 11:12:57 +0900

    fix: onNewIntent's intent is not null

--- mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/MainActivity.kt
+++ mobile/app/src/main/java/co/kr/parkjonghun/whatishedoingwithandroid/mobile/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         // Unreachable.
         Process.killProcess(Process.myPid())
