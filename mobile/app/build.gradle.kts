ba43ee5e499ab6f6aa412d96c70c672655a74381
Park JongHun <thesn515@gmail.com>
2024-06-01 16:05:12 +0900

    chore: stable version 0.2.3

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "co.kr.parkjonghun.whatishedoingwithandroid.mobile"
         versionCode = 1
-        versionName = "0.2.2"
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
