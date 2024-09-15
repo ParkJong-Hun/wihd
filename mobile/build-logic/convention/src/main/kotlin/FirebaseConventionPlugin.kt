a68a26ddcd53cc42d8ec5bd13ff690d2410f9232
Park JongHun <thesn515@gmail.com>
2024-09-15 11:42:37 +0900

    chore(deps): update crashlytics to v3 (major)

--- mobile/build-logic/convention/src/main/kotlin/FirebaseConventionPlugin.kt
+++ mobile/build-logic/convention/src/main/kotlin/FirebaseConventionPlugin.kt
@@ -1,4 +1,3 @@
-import dsl.android
 import dsl.implementation
 import dsl.implementationPlatform
 import dsl.library
@@ -11,18 +10,6 @@ import org.gradle.kotlin.dsl.dependencies
 class FirebaseConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            with(pluginManager) {
-                apply("com.google.gms.google-services")
-                apply("com.google.firebase.crashlytics")
-            }
-
-            android {
-                packagingOptions {
-                    resources {
-                        excludes += "META-INF/gradle/incremental.annotation.processors"
-                    }
-                }
-            }
             dependencies {
                 implementationPlatform(libs.library("firebase-bom"))
                 implementation(libs.library("firebase-common"))
